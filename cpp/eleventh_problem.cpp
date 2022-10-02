366fad8c8998e8433d0ddfffc9eb6f5b3bc512d0
aliammari <100803794+aliammari@users.noreply.github.com>
2022-10-02 19:50:00 +0000

    Strings

---
+++ cpp/eleventh_problem.cpp
@@ -0,0 +1,37 @@
+//first method
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a="",b="";
+    cin >> a >> b;
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    char c = a[0];
+    a[0] = b[0];
+    b[0] = c;
+    cout << a << " " << b << endl;
+    return 0;
+}
+
+
+
+
+
+
+//second method
+/*#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a="",b="";
+    cin >> a >> b;
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    cout << b[0] + a.substr(1,a.size() - 1) << " " << a[0] + b.substr(1,b.size() - 1) << endl;
+    return 0;
+}*/
