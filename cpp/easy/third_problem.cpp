a398205225cc9651e22d7461de43bd2471fe4a73
aliammari <100803794+aliammari@users.noreply.github.com>
2022-10-04 21:13:13 +0000

    Basic Data Types

--- cpp/easy/third_problem.cpp
+++ cpp/easy/third_problem.cpp
@@ -3,8 +3,28 @@
 using namespace std;
 
 
+//first Method
 int main() {
-    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout.precision(3);
+    cout << fixed << d << endl;
+    cout.precision(9);
+    cout << fixed <<  e << endl;
+    return 0;
+}
+
+
+//second Method
+/*
+int main() {
     int a;
     long b;
     char c;
@@ -18,3 +38,4 @@ int main() {
     printf("%.9lf",e);
     return 0;
 }
+*/
