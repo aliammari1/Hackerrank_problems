42ee6a4e1c5dc07bc151844c55057175d9b4d5d2
aliammari <100803794+aliammari@users.noreply.github.com>
2022-10-04 21:35:09 +0000

    Box It!

--- cpp/easy/fifteenth_problem.cpp
+++ cpp/easy/fifteenth_problem.cpp
@@ -33,3 +33,56 @@ class Box {
 //Overload operator << as specified
 //ostream& operator<<(ostream& out, Box& B)
 
+
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
