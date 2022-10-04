73b72eb737990e70ad545ed333aafe60a5673ac4
aliammari <100803794+aliammari@users.noreply.github.com>
2022-10-04 21:50:00 +0000

    Rectangle Area

---
+++ cpp/easy/twentysecond_problem.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+ class Rectangle {
+     protected:
+     int width = 0;
+     int height = 0;
+     public:
+     void display() {
+         cout << width << " " << height << endl;
+     };
+ };
+ 
+ class RectangleArea : public Rectangle{
+     public:
+   void read_input() {
+       cin >> Rectangle::width;
+       cin >> Rectangle::height;
+   };
+   void display() {
+       cout << Rectangle::width * Rectangle::height << endl;  
+   };
+ };
+
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
