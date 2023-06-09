343b1657e6789c41dee6ed276fe1e38f0fa10f69
Ali Ammari <ammari.ali.0001@gmail.com>
2023-06-09 03:53:21 +0100

    Java Stdin and Stdout II

---
+++ java/easy/fourth_problem.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class fourth_problem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        double d = Double.parseDouble(reader.readLine());
+        String s = reader.readLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: "+ x);
+    }
+}
