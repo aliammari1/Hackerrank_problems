b3a330b27cda435964f0a90a722b18a23b703f20
Ali Ammari <ammari.ali.0001@gmail.com>
2023-06-09 03:39:19 +0100

    Java Stdin and Stdout I

---
+++ java/easy/second_problem.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class second_problem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+        int z = Integer.parseInt(reader.readLine());
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
