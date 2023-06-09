5e4b36289762db4fb0c6976fd9c9b83696e0db7a
Ali Ammari <ammari.ali.0001@gmail.com>
2023-06-09 21:12:17 +0100

    Java Loops I

---
+++ java/easy/sixth_problem.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class sixth_problem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
